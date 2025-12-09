29e9f06ba7ecaced6e41f8853f0daaa1b3379473
k220volt <k220volt@hanmail.net>
2025-12-09 21:17:42 +0900

    적우 홈페이지 최초등록

---
+++ js/jwjs.js
@@ -0,0 +1,3 @@
+
+if ( window.location == 'http://atzsemi.com/jw/jw_profile.html' ) 
+{ document.write( '<style>#mainmenu_button1, #mainmenu_button2, #mainmenu_button3 {display:none;}</style>' ); } 
