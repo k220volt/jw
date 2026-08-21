4e11966abf1fa6b81a5d0dd691984abaddb237b3
dongwa78 <dongwa78@naver.com>
2026-08-21 15:13:39 +0900

    html 빌드타입 수정

--- AstroProject/astro.config.mjs
+++ AstroProject/astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: 'file' // 빌드 결과물을 folder/index.html 대신 filename.html 로 생성
+  }
+});
